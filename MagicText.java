b34490372de8a68b6d215f9986bd4af0d38f5a06
Eugene Storm <60484279+ewhitestorm@users.noreply.github.com>
2020-01-31 17:49:16 +0500

    Working version

---
+++ MagicText.java
@@ -0,0 +1,14 @@
+package com.example.shfate;
+
+public class MagicText {
+    final String[] array = {"Yes",
+                            "No",
+                            "I do not know!",
+                            "Absolutely not",
+                            "Probably",
+                            "Ask later",
+                            "Not now",
+                            "Maybe",
+                            "There is a chance",
+                            "Perhaps"};
+}
